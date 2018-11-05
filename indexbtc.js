86ec5794a23405cf72de1a4bba547409ec27f024
yang yang <yangyang@yangdeMacBook-Pro.local>
2018-11-05 16:21:09 +0800

    update

--- indexbtc.js
+++ indexbtc.js
@@ -10,21 +10,22 @@ let Init = async function() {
 
         const root = hdkey.fromMasterSeed(seed);
         const masterPrivateKey = root.privateKey.toString('hex');
-        // console.log(masterPrivateKey);
         
         const addrnode = root.derive("m/44'/60'/0'/0/0");
-        // console.log(addrnode);
 
         const step1 = addrnode._publicKey;
         const step2 = createHash('sha256').update(step1).digest();
+        console.log(step2);
         const step3 = createHash('rmd160').update(step2).digest();
+        console.log(step3);       
 
         var step4 = Buffer.allocUnsafe(21);
+        // add 0x00 for mainnet, add 0x6f for testnet
         step4.writeUInt8(0x00, 0);
         step3.copy(step4, 1); //step4 now holds the extended RIPMD-160 result
         const step9 = bs58check.encode(step4);
         console.log('Base58Check: ' + step9);
-                
+
     } catch (error) {
         console.log(error.stack);
     }
